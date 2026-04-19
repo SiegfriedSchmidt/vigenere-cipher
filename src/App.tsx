90f71be9bb422788183774c39e9ad3a4edc68de3
Matvei <matveiyan@yandex.ru>
2026-04-19 19:22:25 +0300

    change router to hash router, add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {createHashRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import RootLayout from "./layouts/RootLayout.tsx";
 import HomePage from "./pages/HomePage.tsx";
 
-const router = createBrowserRouter(createRoutesFromElements(
+const router = createHashRouter(createRoutesFromElements(
   <Route element={<RootLayout/>}>
     <Route index element={<HomePage/>}/>
   </Route>
